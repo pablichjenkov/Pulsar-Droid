dbddd8b621b79d47c110557b7b0776be2a52dd12
Valdes, Pablo <pvaldes@ncl.com>
2018-09-20 12:26:27 -0400

    Adding UI to message another user or a group chat

--- app/src/main/java/com/ncl/pulsarj/MainActivity.java
+++ app/src/main/java/com/ncl/pulsarj/MainActivity.java
@@ -1,19 +1,16 @@
 package com.ncl.pulsarj;
 
-import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.TextUtils;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,46 +23,92 @@ import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
 
-    // String wsUrl = "ws://192.168.1.79:8080/ws/v2/producer/persistent/public/default/my-topic";
-    String wsUrl = "ws://192.168.1.79:8080/ws/v2/consumer/persistent/public/default/my-topic/my-subs";
+    private String basePubUrl = "ws://192.168.64.102:8080/ws/v2/producer/persistent/public/default/";// sink-topic
+    private String baseSubUrl = "ws://192.168.64.102:8080/ws/v2/consumer/persistent/public/default/";// source-topic/my-subs";
     // String wsUrl = "wss://echo.websocket.org";
 
-    private WebSocket webSocket;
-    private AppCompatEditText outText;
+    private OkHttpClient okHttpClient;
+    private WebSocket pubSocket;
+    private WebSocket subSocket;
+    private AppCompatEditText pubTopic;
+    private AppCompatEditText subTopic;
+    private AppCompatEditText message;
     private TextView inText;
     private int counter = 0;
-    private boolean producer;
-    private boolean automaticAck = true;
-    private String lastMsgId;
+    private String curSinkTopic;
+    private String curSourceTopic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createWebSocket();
+        okHttpClient = createHttpClient();
 
-        outText = findViewById(R.id.outText);
+        pubTopic = findViewById(R.id.pubTopic);
+        subTopic = findViewById(R.id.subTopic);
+        message = findViewById(R.id.message);
         inText = findViewById(R.id.inText);
 
         findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (webSocket != null) {
+                if (pubSocket != null) {
+                    String msg = message.getText().toString() + "_" + counter;
+                    counter ++;
 
-                    if (producer) {
-                        String msg = outText.getText().toString() + "_" + counter;
-                        counter ++;
+                    String msgEncoded = "\"" + Base64.encodeToString(msg.getBytes(), Base64.NO_WRAP | Base64.NO_PADDING) + "\"";
+                    pubSocket.send("{\"payload\":"+ msgEncoded + "}");
 
-                        String msgEncoded = "\"" + Base64.encodeToString(msg.getBytes(), Base64.NO_WRAP | Base64.NO_PADDING) + "\"";
-                        webSocket.send("{\"payload\":"+ msgEncoded + "}");
+                }
+
+            }
+        });
+
+        findViewById(R.id.updateTopic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newSinkTopic = pubTopic.getText().toString();
+                String newSourceTopic = subTopic.getText().toString();
+
+                if (TextUtils.isEmpty(newSinkTopic) || TextUtils.isEmpty(newSourceTopic)) {
+
+                    Toast.makeText(MainActivity.this
+                            , "Missing Topic Config"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+
+                    return;
+                }
+
+                if (newSinkTopic.equals(curSinkTopic) == false) {
+
+                    // Close the old connection if there was one, avoiding leakage connection sockets
+                    if (pubSocket != null) {
+                        pubSocket.close(1000, "Connection changed");
                     }
-                    else {
-                        String ackMsg = "{\"messageId\":" + "\"" + lastMsgId + "\"}";
-                        webSocket.send(ackMsg);
+
+                    // Create the new Socket to Publish to
+                    curSinkTopic = newSinkTopic;
+                    String pubUrl = basePubUrl.concat(newSinkTopic);
+                    pubSocket = createWebSocket(pubUrl, outWsListener);
+                }
+
+                if (newSourceTopic.equals(curSourceTopic) == false) {
+
+                    // Close the old connection if there was one, avoiding leakage connection sockets
+                    if (subSocket != null) {
+                        subSocket.close(1000, "Connection changed");
                     }
 
+                    curSourceTopic = newSourceTopic;
+                    String subUrl = baseSubUrl.concat(newSourceTopic)
+                            .concat("/")
+                            .concat("My-Subscription");
+
+                    subSocket = createWebSocket(subUrl, inWsListener);
                 }
 
             }
@@ -77,17 +120,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        if (webSocket != null) {
-            webSocket.cancel();
-            webSocket.close(1000, "All good");
+        if (pubSocket != null) {
+            pubSocket.cancel();
+            pubSocket.close(1000, "All good");
+        }
+
+        if (subSocket != null) {
+            subSocket.cancel();
+            subSocket.close(1000, "All good");
         }
     }
 
-    private void createWebSocket() {
+    private WebSocket createWebSocket(String wsUrl, WebSocketListener wsListener) {
         Request wsRequest = createRequest(wsUrl);
-        OkHttpClient okHttpClient = createHttpClient();
+        okHttpClient = createHttpClient();
 
-        webSocket = okHttpClient.newWebSocket(wsRequest, wsListener);
+        return okHttpClient.newWebSocket(wsRequest, wsListener);
     }
 
     private OkHttpClient createHttpClient() {
@@ -107,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private WebSocketListener wsListener = new WebSocketListener() {
+    private WebSocketListener inWsListener = new WebSocketListener() {
         @Override
         public void onOpen(WebSocket webSocket, final Response response) {
             super.onOpen(webSocket, response);
@@ -115,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    inText.setText(response.message());
+                    inText.append("in::" + response.message() + "\n");
                     inText.append("\n");
 
                     try {
@@ -136,15 +184,19 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     try {
-                        inText.append("onMessage:\n");
+
+                        String prevText = inText.getText().toString();
+
+                        inText.setText("in::onMessage:\n");
                         inText.append(text);
                         inText.append("\n");
+                        inText.append(prevText);
 
                         JSONObject jMsg = new JSONObject(text);
-                        lastMsgId = jMsg.optString("messageId");
+                        String msgId = jMsg.optString("messageId");
 
-                        if (!TextUtils.isEmpty(lastMsgId) && automaticAck) {
-                            String ackMsg = "{\"messageId\":" + "\"" + lastMsgId + "\"}";
+                        if (!TextUtils.isEmpty(msgId)) {
+                            String ackMsg = "{\"messageId\":" + "\"" + msgId + "\"}";
                             webSocket.send(ackMsg);
                         }
 
@@ -173,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    inText.append("onClosed:\n" + reason);
+                    inText.append("in::onClosed:\n" + reason);
                     inText.append("\n");
                 }
             });
@@ -186,7 +238,100 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    inText.append("onFailure:\n" + t.getMessage());
+                    inText.append("in::onFailure:\n" + t.getMessage());
+                    inText.append("\n");
+                }
+            });
+        }
+
+    };
+
+    private WebSocketListener outWsListener = new WebSocketListener() {
+        @Override
+        public void onOpen(WebSocket webSocket, final Response response) {
+            super.onOpen(webSocket, response);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    inText.append("out::" + response.message() + "\n");
+                    inText.append("\n");
+
+                    try {
+                        inText.append(response.body().string());
+                        inText.append("\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(final WebSocket webSocket, final String text) {
+            super.onMessage(webSocket, text);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        String prevText = inText.getText().toString();
+
+                        inText.setText("out::onMessage:\n");
+                        inText.append(text);
+                        inText.append("\n");
+                        inText.append(prevText);
+
+                        /*
+                        JSONObject jMsg = new JSONObject(text);
+                        String msgId = jMsg.optString("messageId");
+
+                        if (!TextUtils.isEmpty(msgId)) {
+                            String ackMsg = "{\"messageId\":" + "\"" + msgId + "\"}";
+                            webSocket.send(ackMsg);
+                        }
+                        */
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            super.onMessage(webSocket, bytes);
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            super.onClosing(webSocket, code, reason);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket, int code, final String reason) {
+            super.onClosed(webSocket, code, reason);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    inText.append("out::onClosed:\n" + reason);
+                    inText.append("\n");
+                }
+            });
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, final Throwable t, final @Nullable Response response) {
+            super.onFailure(webSocket, t, response);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    inText.append("out::onFailure:\n" + t.getMessage());
                     inText.append("\n");
                 }
             });
